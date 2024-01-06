f1c3a6782bda42d52a9b2be66f4ca3259c77c6b6
vedanti <vedanti.r.bagade@gmail.com>
2024-01-06 19:19:10 +0530

    file updated

--- calc.js
+++ calc.js
@@ -2,3 +2,9 @@ function Solve(val) {
     var v = document.getElementById('res')
     v.value += val
 }
+
+function Result() {
+    var num1 = document.getElementById('res').value
+    var num2 = eval(num1)
+    document.getElementById('res').value = num2
+}
